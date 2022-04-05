d7e3ba9451eb2d3a4c1866cb9d2e24559ec30dcb
sanaullah02 <103077099+sanaullah02@users.noreply.github.com>
2022-04-06 03:44:28 +0500

    Arabic Fonts
    Arabic fonts for business

---
+++ script.js
@@ -0,0 +1,44 @@
+function changeText() {
+		var els = document.getElementsByClassName("ribbon"); for (var i=0; i<els.length; i++) { els[i].style.display = "block"; }
+		var x = document.getElementById("text01"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Arbaeen</p>";
+		var x = document.getElementById("text02"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">AgaGranadaGrntV2Ar</p>";
+		var x = document.getElementById("text03"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">faisalfree</p>";
+		var x = document.getElementById("text04"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Scheherazade</p>";
+		var x = document.getElementById("text05"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">AgaSindibadV2Sndbd</p>";
+		var x = document.getElementById("text06"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">BArshia</p>";
+		var x = document.getElementById("text07"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">ArabicEjaza</p>";
+		var x = document.getElementById("text08"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">alsamt</p>";
+		var x = document.getElementById("text09"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Tabassom</p>";
+		var x = document.getElementById("text10"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">ElMessiri</p>";
+		var x = document.getElementById("text11"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">Abdo</p>";
+		var x = document.getElementById("text12"); x.innerHTML = document.getElementById("sample-name").value+"<br /><p id=\"text00\">NNaskhBook</p>";
+}
+
+function changeToGold() {
+		document.getElementById("text01").style.color = "#DCCD80";
+		document.getElementById("text02").style.color = "#DCCD80";
+		document.getElementById("text03").style.color = "#DCCD80";
+		document.getElementById("text04").style.color = "#DCCD80";
+		document.getElementById("text05").style.color = "#DCCD80";
+		document.getElementById("text06").style.color = "#DCCD80";
+		document.getElementById("text07").style.color = "#DCCD80";
+		document.getElementById("text08").style.color = "#DCCD80";
+		document.getElementById("text09").style.color = "#DCCD80";
+		document.getElementById("text10").style.color = "#DCCD80";
+		document.getElementById("text11").style.color = "#DCCD80";
+		document.getElementById("text12").style.color = "#DCCD80";
+}
+function changeToSilver() {
+		document.getElementById("text01").style.color = "#FFFFFF";
+		document.getElementById("text02").style.color = "#FFFFFF";
+		document.getElementById("text03").style.color = "#FFFFFF";
+		document.getElementById("text04").style.color = "#FFFFFF";
+		document.getElementById("text05").style.color = "#FFFFFF";
+		document.getElementById("text06").style.color = "#FFFFFF";
+		document.getElementById("text07").style.color = "#FFFFFF";
+		document.getElementById("text08").style.color = "#FFFFFF";
+		document.getElementById("text09").style.color = "#FFFFFF";
+		document.getElementById("text10").style.color = "#FFFFFF";
+		document.getElementById("text11").style.color = "#FFFFFF";
+		document.getElementById("text12").style.color = "#FFFFFF";
+}
